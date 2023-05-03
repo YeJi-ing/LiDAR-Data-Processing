e7ffe1cbc48a828f21253678f548ad625f45f10a
Ye Ji <120790768+YeJi-ing@users.noreply.github.com>
2023-05-04 00:10:18 +0900

    Update make_2D_ptCloud.m

--- make_2D_ptCloud.m
+++ make_2D_ptCloud.m
@@ -3,30 +3,29 @@
 %% Data load 
 % 데이터 길이 : 1200, Field of View (FOV) : 300, Angular Resolution : 0.25
 
-clc; clear;
+clc; clear; % 작업 공간 및 명령 창 모두 초기화
 
-lidar_data_1 = readmatrix("map\1.csv");
-lidar_data_2 = readmatrix("map\2.csv");
+lidar_data = readmatrix("R300_Data.csv"); % N(row) x 1200(column) double
+lidar_data_1 = lidar_data(1,:); % 1(row) x 1200(column) double
+
+angle_start = 0; % 0 ~ 299.75
+angle_res1 = 0.25;
+angle1 = angle_start : angle_res1 : angle_start + angle_res1 * 1199;
 
 %% make ptClouds
-filelist = {lidar_data_1, lidar_data_2};
+x1 = cosd(angle1).*(lidar_data_1);
+y1 = sind(angle1).*(lidar_data_1);
+z1 = zeros(1, length(x1));
 
-for i=1:length(filelist)
-    lidar_data = filelist{i}(i,2:1201);
+%% print ptClouds
+figure; 
+axis([0 20 -10 10]); 
+plot3(x1, y1, z1, 'r.'); xlabel('x(m)'); ylabel('y(m)');
 
-    angle_start = 0; % 0 ~ 299.75
-    angle_res1 = 0.25;
-    angle1 = angle_start : angle_res1 : angle_start + angle_res1 * 1199;
-    
-    x1 = cosd(angle1).*(lidar_data);
-    y1 = sind(angle1).*(lidar_data);
-    z1 = zeros(1, length(x1));
-    
-    xyz = [x1; y1; z1;];
-    xyz = xyz.';
-    % xyzs{1, i} = xyz;
+%% make MATLAB's ptClouds
+xyz = [x1; y1; z1;];
+xyz = xyz.';
 
-    ptClouds{1, i} = pointCloud(xyz,Color=[255 255 255]);
-end
+ptClouds{1, 1} = pointCloud(xyz,Color=[255 255 255]);
 
 save('ptClouds.mat','ptClouds');
