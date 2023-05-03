6e7ea64cf70612d64a0615c7a711b95fd2b0d549
Ye Ji <120790768+YeJi-ing@users.noreply.github.com>
2023-05-04 02:11:10 +0900

    convert raw data to MATLAB's point cloud format

--- make_2D_ptCloud.m
+++ make_2D_ptCloud.m
@@ -1,14 +1,18 @@
 %% 2D LiDAR
+% LiDAR specification :
+% Channels : 1Ch
+% FOV : Horizontal 300°
+% Angular Resolution : Horizontal 0.25°
 
 %% Data load 
-% 데이터 길이 : 1200, Field of View (FOV) : 300, Angular Resolution : 0.25
 
 clc; clear; % 작업 공간 및 명령 창 모두 초기화
 
 lidar_data = readmatrix("filename"); % N(row) x 1200(column) double
 lidar_data_1 = lidar_data(1,:); % 1(row) x 1200(column) double
 
-angle_start = 0; % 0 ~ 299.75
+% (H) 0° ~ 300°
+angle_start = 0;
 angle_res1 = 0.25;
 angle1 = angle_start : angle_res1 : angle_start + angle_res1 * 1199;
 
