edfa6966665982d6409fa20d83a8abe7a4dd3688
Ye Ji <120790768+YeJi-ing@users.noreply.github.com>
2023-05-04 02:08:10 +0900

    convert raw data to MATLAB's point cloud format

--- make_3D_ptCloud.m
+++ make_3D_ptCloud.m
@@ -1,45 +1,48 @@
 %% 3D LiDAR
+% LiDAR specification :
+% Channels : 4Ch
+% FOV : Horizontal 100° / Vertical 3.2°
+% Angular Resolution : Horizontal 0.25° / Vertical 1.07°
 
 %% Data load
-% 데이터 길이 : 400, 수직 분해능 : 1.07, 수평 분해능 : 0.25
 
 clear; clc; % 작업 공간 및 명령 창 초기화
 
 lidar_data = readmatrix("filename"); % N(row) x 400(column) double
-lidar_data_1 = lidar_data(1,:); % <ch1> 1(row) x 400(column) double
-lidar_data_2 = lidar_data(2,:); % <ch2> 1(row) x 400(column) double
-lidar_data_3 = lidar_data(3,:); % <ch3> 1(row) x 400(column) double
-lidar_data_4 = lidar_data(4,:); % <ch4> 1(row) x 400(column) double
+lidar_data_1 = lidar_data(1,:); % <Ch1> 1(row) x 400(column) double
+lidar_data_2 = lidar_data(2,:); % <Ch2> 1(row) x 400(column) double
+lidar_data_3 = lidar_data(3,:); % <Ch3> 1(row) x 400(column) double
+lidar_data_4 = lidar_data(4,:); % <Ch4> 1(row) x 400(column) double
 
-% (H) 0 ~ 99.75
+% (H) 0° ~ 100°
 angle_start = 0;
 angle_res_H = 0.25;
 angle_H = angle_start : angle_res_H : angle_start + angle_res_H * 399;
 
-% (V) -1.6 ~ 1.6
-angle_1V = -1.6;
-angle_2V = -0.8;
-angle_3V =  0.8;
-angle_4V =  1.6;
+% (V) -1.07° ~ 2.14°
+angle_1V = -1.07;
+angle_2V =     0;
+angle_3V =  1.07;
+angle_4V =  2.14;
 
 %% make ptClouds
 
-% ch1
+% Ch1
 x{1} = cosd(angle_1V)*sind(angle_H).*(lidar_data_1);
 y{1} = cosd(angle_1V)*cosd(angle_H).*(lidar_data_1);
 z{1} = sind(angle_1V).*(lidar_data_1);
 
-% ch2
+% Ch2
 x{2} = cosd(angle_2V)*sind(angle_H).*(lidar_data_2);
 y{2} = cosd(angle_2V)*cosd(angle_H).*(lidar_data_2);
 z{2} = sind(angle_2V).*(lidar_data_2);
 
-% ch3
+% Ch3
 x{3} = cosd(angle_3V)*sind(angle_H).*(lidar_data_3);
 y{3} = cosd(angle_3V)*cosd(angle_H).*(lidar_data_3);
 z{3} = sind(angle_3V).*(lidar_data_3);
 
-% ch4
+% Ch4
 x{4} = cosd(angle_4V)*sind(angle_H).*(lidar_data_4);
 y{4} = cosd(angle_4V)*cosd(angle_H).*(lidar_data_4);
 z{4} = sind(angle_4V).*(lidar_data_4);
